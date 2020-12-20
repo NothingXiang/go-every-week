418d729e8c5573a23ed0e6cbee16269be4b51b10
wenxinxiang <wenxinxiang@tencent.com>
2020-12-20 15:56:03 +0800

    feat(gee-demo): usage of esay http server

---
+++ gee-demo/go.mod
@@ -0,0 +1,7 @@
+module github.com/NothingXiang/go-every-week/gee-demo
+
+replace gee => ../gee
+
+require gee v0.0.0-00010101000000-000000000000
+
+go 1.13
---
+++ gee-demo/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"gee"
+)
+
+func main() {
+	r := gee.New()
+
+	r.GET("/", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+	})
+
+	r.GET("/hello", func(c *gee.Context) {
+		// expect /hello?name=geektutu
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.URL.Path)
+	})
+	r.POST("/login", func(c *gee.Context) {
+		c.JSON(http.StatusOK, gee.H{
+			"username": c.PostForm("username"),
+			"password": c.PostForm("password"),
+		})
+	})
+
+	if err := r.Run(":8000"); err != nil {
+		fmt.Println(err)
+	}
+}
